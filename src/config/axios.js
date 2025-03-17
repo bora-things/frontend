cb4511805c4ba1c0aa62b3bffc3bfb335919893b
MatheusSCristo <cristo.matheus@outlook.com>
2025-03-17 20:02:14 -0300

    feat: Add axios config

---
+++ src/config/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  withCredentials: true,
+  redirect: "follow"
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (window.location.pathname !== '/') {
+        window.location.href = '/'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
