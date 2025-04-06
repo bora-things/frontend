2ccbc3b9273f554e89a45d55a9a2866afbefcf6b
Matheus Cristo <cristo.matheus@outlook.com>
2025-04-06 11:45:49 -0300

    feat: Add subjects interests on profile

--- src/components/SubjectCard.vue
+++ src/components/SubjectCard.vue
@@ -1,78 +1,62 @@
 <script setup>
-const props = defineProps(['subject', 'interest'])
-const { subject, interest } = props
-const { code, type, name, teacher, time } = subject
-</script>
+import { ClockCircleOutlined, UserOutlined } from '@ant-design/icons-vue'
 
-<script>
-import { ClockCircleOutlined, UserOutlined } from '@ant-design/icons-vue';
-import { Avatar, Tooltip } from 'ant-design-vue';
+const props = defineProps(['classSubject', 'interest'])
+const { code, type, name, teacher, time, friendsInClass } = props.classSubject
 
-export default {
-  components: {
-    'a-avatar': Avatar,
-    'a-avatar-group': Avatar.Group,
-    'a-tooltip': Tooltip,
-    UserOutlined: UserOutlined,
-    ClockCircleOutlined: ClockCircleOutlined
-  },
-  data() {
-    return {
-      users: [
-        { id: 1, name: 'Usuário 1', avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=1' },
-        { id: 2, name: 'Usuário 2', avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=2' },
-        { id: 3, name: 'Usuário 3', avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=3' },
-        { id: 4, name: 'Usuário 4', avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=4' },
-        { id: 5, name: 'Usuário 5', avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=5' },
-        { id: 6, name: 'Usuário 6', avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=6' }
-      ],
-      maxVisible: 4
-    }
-  },
-  computed: {
-    visibleUsers() {
-      return this.users.slice(0, this.maxVisible)
-    },
-    hiddenUsersCount() {
-      return this.users.length - this.maxVisible
-    },
-    typeClass() {
-      return this.subject.type === 'Obrigatória'
-        ? 'bg-bp_yellow-200/20 text-bp_yellow-100'
-        : this.interest?'bg-blue-500/90':'bg-blue-500/20 text-blue-500'
-    }
-  }
-}
+const maxVisible = 4
+const visibleUsers = friendsInClass?.slice(0, maxVisible) || []
+const hiddenUsersCount = friendsInClass?.length - maxVisible || 0
 </script>
 
 <template>
   <div
     :class="[
-      'w-full  rounded-md flex flex-col gap-2 p-4',
+      'w-full rounded-md flex flex-col gap-2 p-4',
       interest ? 'bg-white/60 text-black' : 'bg-bp_neutral-700'
     ]"
   >
-    <div class="flex justify-between items-center ">
+    <div class="flex justify-between items-center">
       <h1 class="text-xl font-bold">{{ code }}</h1>
-      <span class="justify-self-end text-sm px-3 py-1 rounded-2xl font-semibold" :class="typeClass">
+      <span
+        v-if="!interest"
+        class="justify-self-end text-sm px-3 py-1 rounded-2xl font-semibold"
+        :class="[
+          type === 'Obrigatória'
+            ? 'bg-bp_yellow-200/20 text-bp_yellow-100'
+            : 'bg-blue-200/50 text-blue-100'
+        ]"
+      >
         {{ type }}
       </span>
     </div>
     <div class="flex flex-col gap-2">
       <p>{{ name }}</p>
-      <div class="flex gap-4 items-center">
+      <div v-if="!interest" class="flex gap-4 items-center">
         <UserOutlined />
         <p>{{ teacher }}</p>
       </div>
-      <div class="flex gap-4 items-center">
+      <div v-if="!interest" class="flex gap-4 items-center">
         <ClockCircleOutlined />
         <p>{{ time }}</p>
       </div>
     </div>
-    <div>
+    <div class="flex gap-4 items-center mt-2">
       <a-avatar-group>
-        <a-tooltip v-for="user in visibleUsers" :key="user.id" :title="user.name" placement="top">
-          <a-avatar :src="user.avatar" />
+        <a-tooltip
+          v-for="user in visibleUsers"
+          :key="user.id"
+          :title="user.name"
+          placement="top"
+          class="mx-2"
+        >
+          <a-avatar v-if="user.avatar" :src="user.avatar" />
+          <div
+            v-if="!user.avatar"
+            class="bg-bp_neutral-600 rounded-full flex items-center justify-center w-8 h-8"
+          >
+            <UserOutlined class="text-xl text-white" />
+          </div>
         </a-tooltip>
 
         <a-avatar v-if="hiddenUsersCount > 0" style="background-color: #f56a00">
