00d404cfc39d684f0c2c7ab117d1a90e5abedf10
Matheus Cristo <cristo.matheus@outlook.com>
2025-04-07 22:37:23 -0300

    feat: Submit search on enter

--- src/pages/Friends/FriendsView.vue
+++ src/pages/Friends/FriendsView.vue
@@ -1,50 +1,53 @@
 <script setup>
 import BpButton from '@/components/BpButton.vue'
 import BpPagination from '@/components/BpPagination.vue'
-import ListItemFriend from '@/components/ListItemFriend.vue'
 import InputSearch from '@/components/InputSearch.vue'
-import { onMounted, ref, computed } from 'vue'
+import ListItemFriend from '@/components/ListItemFriend.vue'
+import { computed, onMounted, ref } from 'vue'
 import { getFriends } from './FriendsController'
 
-const friends = ref([]);
-const currentPagination = ref(0);
-const searchQuery = ref('');
-const selectTypeFriends = ref('Todos');
+const friends = ref([])
+const currentPagination = ref(0)
+const searchQuery = ref('')
+const selectTypeFriends = ref('Todos')
 
 function updateCurrentPagination(newValue) {
-  currentPagination.value = newValue;
+  currentPagination.value = newValue
 }
 
 onMounted(async () => {
-    friends.value = await getFriends(); 
-});
+  friends.value = await getFriends()
+})
 
 const updateSearchQuery = (query) => {
-  searchQuery.value = query;
-};
+  searchQuery.value = query
+}
 
 const filteredFriends = computed(() => {
-  return friends.value.filter(friend => {
+  return friends.value.filter((friend) => {
     const matchesTypeFriends =
-      selectTypeFriends.value === 'Todos'
-      || (selectTypeFriends.value === 'same-course' && friend.of_my_course === 1)
-      || (selectTypeFriends.value === 'favorites' && friend.favorite === 1);
+      selectTypeFriends.value === 'Todos' ||
+      (selectTypeFriends.value === 'same-course' && friend.of_my_course === 1) ||
+      (selectTypeFriends.value === 'favorites' && friend.favorite === 1)
 
     const matchesSearchQuery =
-      !searchQuery.value 
-      || friend.name.toLowerCase().includes(searchQuery.value.toLowerCase()) 
-      || friend.id.toString().includes(searchQuery.value);
+      !searchQuery.value ||
+      friend.name.toLowerCase().replace(' ', '').includes(searchQuery.value.toLowerCase()) ||
+      friend.id.toString().replace(' ', '').includes(searchQuery.value)
 
-    return matchesTypeFriends && matchesSearchQuery;
-  });
-});
+    return matchesTypeFriends && matchesSearchQuery
+  })
+})
 
 const paginatedFriends = computed(() => {
-  const start = currentPagination.value * 14;
-  const end = start + 14;
-  return filteredFriends.value.slice(start, end);
-});
+  const start = currentPagination.value * 14
+  const end = start + 14
+  return filteredFriends.value.slice(start, end)
+})
 
+const handleSearch = () => {
+  updateSearchQuery(searchQuery.value);
+};
 </script>
 
 <template>
@@ -54,16 +57,15 @@ const paginatedFriends = computed(() => {
         <h2 class="text-3xl font-bold border-b border-bp_neutral-800 pb-6 mb-6">amigos</h2>
         <div class="">
           <div class="flex flex-wrap gap-4 py-4 justify-end md:justify-between">
-            <InputSearch
-            @search="updateSearchQuery"
-            v-model="searchQuery" class="w-full lg:w-1/2" />
+            <form @submit.prevent="handleSearch" class="w-full lg:w-1/2">
+              <InputSearch
+                @search="updateSearchQuery"
+                v-model="searchQuery"
+              />
+            </form>
 
             <div class="">
-              <select
-                v-model="selectTypeFriends"
-                name="filter"
-                class="card-options text-lg"
-                >
+              <select v-model="selectTypeFriends" name="filter" class="card-options text-lg">
                 <option value="Todos">Todos</option>
                 <option value="same-course">Do meu curso</option>
                 <option value="favorites">Favoritos</option>
@@ -77,11 +79,7 @@ const paginatedFriends = computed(() => {
         </div>
 
         <div class="grid grid-cols-1 gap-4 mt-6 lg:grid-cols-2">
-          <ListItemFriend
-            v-for="friend in paginatedFriends"
-            :key="friend.id"
-            :friend="friend"
-          />
+          <ListItemFriend v-for="friend in paginatedFriends" :key="friend.id" :friend="friend" />
         </div>
       </div>
       <div class="flex justify-center mt-12 pb-7">
