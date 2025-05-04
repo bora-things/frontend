d09eddae582f4e105e6bedb07a585f0a271a044b
NatalyaNunes <nunes.natalya23@gmail.com>
2025-05-04 08:29:43 -0300

    add components

---
+++ src/components/CardAd.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-72 md:w-96 bg-bp_neutral-700 p-4 rounded-lg m-6">
+        <header class="flex justify-between items-center mb-4">
+            <p class="title-h1">Anúncios</p>
+            <span class="text-sm text-bp_neutral-600">ver todos</span>
+        </header>
+        <div class="flex items-center gap-2">
+            <div class="bg-bp_neutral-900 rounded-xl border border-bp_neutral-600 p-2">
+                <v-icon name="md-notificationsnone-outlined" scale="1.5"></v-icon>
+            </div>
+            <div>
+                <p>Manutenção do sistema</p>
+                <span class="text-sm text-bp_neutral-500">O sistema ficará pausado dia 23.05 a partir das 15h</span>
+            </div>
+        </div>
+        <div class="flex justify-center mt-6">
+            <v-icon name="md-expandcircledown-outlined" scale="1.5"></v-icon>
+        </div>
+    </div>
+</template>
---
+++ src/components/PersonalizedCalendar.vue
@@ -0,0 +1,26 @@
+<script>
+import "cally";
+</script>
+
+<template>
+    <calendar-date class="cally bg-bp_neutral-900 border border-base-300 shadow-lg rounded-box">
+        <svg aria-label="Previous" class="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
+        <svg aria-label="Next" class="fill-current size-4" slot="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
+        <calendar-month></calendar-month>
+    </calendar-date>
+</template>
+
+<style>
+.cally ::part(button day today){
+    background-color: #01D600 !important;
+}
+.cally ::part(selected){
+    border:2px dashed #F80052 !important;
+    background-color: transparent !important;
+    color: #FFFFFF !important;
+}
+.cally ::part(day){
+    border-radius: 100%;
+    border: none;
+}
+</style>
