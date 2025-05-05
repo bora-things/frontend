59af304cf911ac3af435727379413fb59ccf77de
Matheus Cristo <cristo.matheus@outlook.com>
2025-05-05 15:44:44 -0300

    refact: Change subjectcard component

--- src/components/SubjectCardEC.vue
+++ src/components/SubjectCardEC.vue
@@ -1,14 +1,5 @@
 <script setup>
-const props = defineProps(['classSubject', 'interest'])
-const { ano, 'id-turma': IdTurma, periodo, component, friends } = props.classSubject
-
-const testFriends = friends?.[0]
-  ? Array(10).fill(friends[0]) // Repete o primeiro amigo 10 vezes
-  : [] //
-
-const maxVisible = 4
-const visibleUsers = testFriends?.slice(0, maxVisible) || []
-const hiddenUsersCount = testFriends?.length - maxVisible || 0
+const props = defineProps(['component'])
 
 const formatUserName = (name) => {
   const nameParts = name.split(' ')
@@ -22,27 +13,7 @@ const formatUserName = (name) => {
 <template>
   <div class="bg-[#020202] w-full rounded-md flex flex-col gap-6 p-4 text-vtd-secondary-100">
     <p>{{ component.nome }}</p>
-    <div class="flex justify-between items-end">
-      <div>
-        <div v-if="visibleUsers.length > 0" class="flex gap-2 items-center">
-          <div v-for="friend in visibleUsers" className="tooltip  rounded-full tooltip-bottom ">
-            <div className="tooltip-content text-xs tooltip-info  tooltip-bottom">
-              <div className="text-white">
-                <span>{{ formatUserName(friend.name) }}</span>
-              </div>
-            </div>
-            <div :key="friend.id">
-              <img class="w-8 h-8 rounded-full" :src="friend.imageUrl" />
-            </div>
-          </div>
-          <div class="w-8 h-8 rounded-full bg-[#fdfdfd] flex items-center justify-center text-black">
-            +{{ hiddenUsersCount }}
-          </div>
-        </div>
-        <div v-else class="border p-2 rounded-box text-xs border-dotted border-white">
-          <span>SEM AMIGOS AQUI {{ ':(' }}</span>
-        </div>
-      </div>
+    <div class="flex justify-between items-end self-end">
       <div class="flex flex-col gap-2 items-end font-medium">
         <span class="badge bg-transparent border text-vtd-secondary-100">{{
           component.codigo
--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,41 +1,49 @@
 <script setup>
+import InputSearch from '@/components/InputSearch.vue'
+import SubjectCard from '@/components/SubjectCard.vue'
 import SubjectCardEC from '@/components/SubjectCardEC.vue'
 import api from '@/config/axios.config'
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 
 const classes = ref(null)
 const selectedClasses = ref([])
 const periods = ref([])
 const components = ref([
   {
-    name: 'CÁLCULO DIFERENCIAL E INTEGRAL I',
-    workload: 90,
-    code: 'IMD0024',
-    friends: [
-      {
-        name: 'Lucas',
-        imageUrl: ''
-      },
-      {
-        name: 'Lucas',
-        imageUrl: ''
-      },
-      {
-        name: 'Lucas',
-        imageUrl: ''
-      }
-    ]
+    nome: 'CÁLCULO DIFERENCIAL E INTEGRAL I',
+    'carga-horaria-total': 90,
+    codigo: 'IMD0024',
+    obrigatoria: true
+  },
+  {
+    nome: 'CÁLCULO DIFERENCIAL E INTEGRAL I',
+    'carga-horaria-total': 90,
+    codigo: 'IMD0024',
+    obrigatoria: true
+  },
+  {
+    nome: 'CÁLCULO DIFERENCIAL E INTEGRAL I',
+    'carga-horaria-total': 90,
+    codigo: 'IMD0024',
+    obrigatoria: true
+  },
+  {
+    nome: 'CÁLCULO DIFERENCIAL E INTEGRAL I',
+    'carga-horaria-total': 90,
+    codigo: 'IMD0024',
+    obrigatoria: true
   }
 ])
+const componentsFiltered = ref([...components.value])
 const searchQuery = ref('')
 const selectedPeriod = ref('')
+
+const componentType = ref('TODAS')
 
 function selectPeriod(period) {
   selectedPeriod.value = period
   const periodClasses = classes.value[period]
-  console.log(periodClasses)
   selectedClasses.value = periodClasses ? periodClasses.map((item) => ({ ...item })) : []
-  console.log(selectedClasses.value)
 }
 
 async function fetchClasses() {
@@ -77,6 +85,28 @@ const selectedPeriodWorkload = computed(() =>
     0
   )
 )
+
+watch(
+  () => componentType.value,
+  (newValue) => {
+    console.log(components.value)
+    if (newValue === 'OBRIGATORIO') {
+      componentsFiltered.value = components.value.filter((item) => item.obrigatoria)
+    } else if (newValue === 'OPTATIVO') {
+      componentsFiltered.value = components.value.filter((item) => !item.obrigatoria)
+    } else {
+      componentsFiltered.value = components
+    }
+  }
+)
+
+function toggleComponentType(type) {
+  if (componentType.value === type) {
+    componentType.value = 'TODAS' // Reseta para "TODAS" se o mesmo botão for clicado novamente
+  } else {
+    componentType.value = type // Define o tipo selecionado
+  }
+}
 </script>
 
 <template>
@@ -103,7 +133,7 @@ const selectedPeriodWorkload = computed(() =>
         <div className="tooltip">
           <div className="tooltip-content text-xl tooltip-error tooltip-bottom">
             <div className="text-white ">
-              <span >Limite alcançado</span>
+              <span>Limite alcançado</span>
             </div>
           </div>
           <span class="title-h2"> {{ selectedPeriodWorkload }}h </span>
@@ -111,47 +141,61 @@ const selectedPeriodWorkload = computed(() =>
       </header>
 
       <section class="grid md:grid-cols-3 bg-[#2F2F2F] rounded-md gap-4 p-4" :key="selectedPeriod">
-        <SubjectCardEC
+        <SubjectCard
           v-for="item in selectedClasses"
           :key="item.code"
           class="w-full"
           :classSubject="item"
         />
       </section>
-
-      <!-- <div class="flex items-center justify-between lg:flex-row gap-3 p-2">
-        <div class="flex flex-col md:flex-row md:justify-between items-center gap-5">
-          <p class="title-h1">Estrutura Curricular</p>
-          <InputSearch @search="updateSearchQuery" v-model="searchQuery" class="" />
-        </div>
-        <div class="flex justify-end space-x-4 p-2 rounded-xl bg-bp_neutral-700 w-fit h-fit">
-          <div class="flex items-center space-x-2">
-            <select
-              v-model="selectedPeriod"
-              class="border-bp_green-600 card-options rounded-xl text-lg"
-            >
-              <option hidden value="Periodo">Período</option>
-              <option value="1">1° Periodo</option>
-              <option value="2">2° Periodo</option>
-              <option value="3">3° Periodo</option>
-            </select>
+      <div class="flex flex-col  mt-6">
+        <div class="flex items-center justify-between lg:flex-row gap-3 p-2">
+          <div class="flex flex-col md:flex-row md:justify-between items-center gap-5">
+            <p class="title-h1">Estrutura Curricular</p>
+            <InputSearch
+              @search="updateSearchQuery"
+              v-model="searchQuery"
+              class=""
+              placeholder="Pesquisar matérias"
+            />
           </div>
-          <div class="flex items-center space-x-2">
-            <button class="card-options rounded-xl border border-bp_primary-500">Optativa</button>
+          <div class="flex justify-end space-x-4 p-2 rounded-xl bg-bp_neutral-700 w-fit h-fit">
+            <div class="flex items-center space-x-2">
+              <button
+                :class="[
+                  'card-options rounded-xl border border-bp_green-600',
+                  componentType === 'OBRIGATORIO' ? 'bg-bp_green-600' : ''
+                ]"
+                @click="toggleComponentType('OBRIGATORIO')"
+              >
+                Obrigatório
+              </button>
+            </div>
+            <div class="flex items-center space-x-2">
+              <button
+                :class="[
+                  'card-options rounded-xl border border-bp_primary-500',
+                  componentType === 'OPTATIVO' ? 'bg-bp_primary-500' : ''
+                ]"
+                @click="toggleComponentType('OPTATIVO')"
+              >
+                Optativo
+              </button>
+            </div>
           </div>
         </div>
-      </div> -->
 
-      <!-- <section
-        class="max-w-full my-6 grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2"
-      >
-        <SubjectCardEC
-          class="w-full"
-          v-for="subject in lastPeriod.classes"
-          :key="subject.code"
-          :classSubject="subject"
-        />
-      </section> -->
+        <section
+          class="max-w-full my-6 grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 gap-2 bg-[#2F2F2F] p-4"
+        >
+          <SubjectCardEC
+            class="w-full"
+            v-for="component in componentsFiltered"
+            :key="component.code"
+            :component="component"
+          />
+        </section>
+      </div>
     </main>
   </div>
 </template>
