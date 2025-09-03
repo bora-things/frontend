7b94a98134545c9b4205886760c80eea9ecce031
MatheusSCristo <cristo.matheus@outlook.com>
2025-09-03 20:26:06 -0300

    refact(capitalizeText): Preserve uppercase for Roman numerals

--- src/utils/capitalizeText.js
+++ src/utils/capitalizeText.js
@@ -1,10 +1,14 @@
 export function capitalizeText(text) {
-  if(!text || typeof text !== 'string') {
-    return '';
+  if (!text || typeof text !== 'string') {
+    return ''
   }
-  const words=text.toLowerCase().split(" ");
-  const capitalizedWords=words.map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  });
-  return capitalizedWords.join(" ");
+  const romanRegex = /^(?=[MDCLXVI])M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/i
+  const words = text.toLowerCase().split(' ')
+  const capitalizedWords = words.map((word) => {
+    if (romanRegex.test(word.toUpperCase())) {
+      return word.toUpperCase()
+    }
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  })
+  return capitalizedWords.join(' ')
 }
