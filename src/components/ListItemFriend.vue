8a97d69ce582e30751ec872bd5134fdb94b8d030
NatalyaNunes <nunes.natalya23@gmail.com>
2025-09-26 17:44:31 -0300

    interface update

--- src/components/ListItemFriend.vue
+++ src/components/ListItemFriend.vue
@@ -1,35 +1,42 @@
 <script setup>
 
 const props = defineProps(['friend'])
-const { id, name, degree, period } = props.friend
+const { id, name, degree, period, is_friend } = props.friend
+
 </script>
 
 <template>
   <a
     :href="`/perfil/${id}`"
-    class="hover:bg-bp_neutral-600 user-container bg-bp_neutral-700 rounded-lg h-28 text-white flex justify-between items-center px-2 md:px-6"
+    class="border border-bp_grayscale-500 hover:border-bp_green-400 user-container bg-bp_neutral-800 rounded-xl h-28 text-white flex justify-between items-center px-2 md:px-6 transition-colors duration-500 ease-in-out"
   >
-    <div class="flex justify-between items-center gap-3">
+    <div class="flex items-center gap-3">
       <img
         class="rounded-full w-[73px] h-[73px]"
         src="https://images.pexels.com/photos/21085439/pexels-photo-21085439/free-photo-of-preto-e-branco-p-b-olhando-vendo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="foto do amigo"
       />
-      <div class="user-data">
+      <div>
         <div class="text-xl font-bold pb-2">{{ name }}</div>
-        <div class="flex gap-2">
+        <div class="flex  flex-wrap gap-2">
           <span class="inline-flex items-center gap-1">
-            <v-icon class="text-bp_primary-100" name="md-school-outlined"></v-icon>
-            {{ degree }}</span
-          >
+            <v-icon class="text-bp_green-400" name="md-school-outlined"></v-icon>
+            {{ degree }}
+          </span>
           <span class="inline-flex items-center gap-1">
-            <v-icon class="text-bp_primary-100" name="md-calendartoday-outlined"></v-icon> Período
-            {{ period }}°</span
-          >
+            <v-icon class="text-bp_green-400" name="md-calendartoday-outlined"></v-icon>
+            Período {{ period }}°
+          </span>
         </div>
       </div>
     </div>
 
-    <span class="badge bg-bp_neutral-300 border-none">8 minutos atrás</span>
+    <div v-if="!is_friend" class="flex items-center">
+      <button class="bg-bp_green-400 hover:bg-bp_green-600 font-bold px-3 py-2 rounded-md transition-colors duration-500 ease-in-out"><v-icon name="md-personaddalt-round" scale="1.2"/>  Adicionar</button>
+    </div>
+
+    <div v-if="is_friend" class="flex items-center">
+      <button class="border-2 border-bp_grayscale-500 hover:bg-bp_grayscale-500 font-bold px-3 py-2 rounded-md transition-colors duration-500 ease-in-out"><v-icon name="md-personremove-round"/>  Remover</button>
+    </div>
   </a>
 </template>
